9694142d05401060c7c1ff711467cbca677484b3
Eisen-W <nseisenw+github@protonmail.com>
2025-12-07 23:21:55 +0900

    feat: colored window

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 
 int main()
 {
-    auto posUndefined = SDL_WINDOWPOS_UNDEFINED;
-    SDL_CreateWindow("IMG-viewer", posUndefined, posUndefined, 800, 600, SDL_WINDOW_RESIZABLE);
-    std::cout << "IMG-viewer" << std::endl;
+    // std::cout << "IMG-viewer" << std::endl;
+    auto posUndef = SDL_WINDOWPOS_UNDEFINED;
+    SDL_Window *pwin = SDL_CreateWindow("IMG-viewer", posUndef, posUndef, 800, 600, SDL_WINDOW_RESIZABLE);
+    SDL_Surface *psurface =  SDL_GetWindowSurface(pwin);
+    SDL_FillRect(psurface, NULL, 0xFF0000FF);
+    SDL_UpdateWindowSurface(pwin);
+
+    SDL_Delay(5000);
+    SDL_Quit();
     return 0;
 }
